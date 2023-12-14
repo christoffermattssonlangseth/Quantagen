0bc93ced2ceda94eba4c1886968c0703d4dc0d0d
Christoffer Mattsson Langseth <christoffer.langseth@scilifelab.se>
2023-12-14 13:50:46 +0100

    Remove build, dist, and egg-info directories

--- build/lib/quantagen/__init__.py
+++
@@ -1,3 +0,0 @@
-from .core import countagen_main, countagen_main
-from .utils import extract_metrics, add_quality
-from .image_processing import get_image_dimensions, countagen_projection, group_channels
